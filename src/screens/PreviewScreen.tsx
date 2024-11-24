7f88a7e66372f00e257bef12a51e71a410e800ac
programmer-rahul <7678478061k@gmail.com>
2024-11-24 13:57:06 +0530

    added pagerview

--- src/screens/PreviewScreen.tsx
+++ src/screens/PreviewScreen.tsx
@@ -1,41 +1,70 @@
 import {Image, Pressable, Text, View} from 'react-native';
 import {useWallpaper} from '../context/WallpaperContext';
+import PagerView from 'react-native-pager-view';
+import {useState} from 'react';
+import {TWallpaper} from '../types/wallpaper';
 
 const PreviewScreen = () => {
-  const {selectedPreviewWallpaper} = useWallpaper();
+  const {selectedPreviewWallpaper, wallpapersList} = useWallpaper();
+
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [wallpapersListing, setWallpapersListing] =
+    useState<TWallpaper[]>(wallpapersList);
 
   return selectedPreviewWallpaper ? (
     <View
       style={{
         paddingVertical: 10,
-        paddingHorizontal: 20,
         backgroundColor: '#999999',
         flex: 1,
         flexDirection: 'column',
         rowGap: 20,
       }}>
-      <View style={{flexDirection: 'row', backgroundColor: 'transparent'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: 'transparent',
+          paddingHorizontal: 20,
+        }}>
         <View style={{flex: 1}}>
-          <Text>{selectedPreviewWallpaper.title}</Text>
-          <Text>Views : {selectedPreviewWallpaper.viewsCount}</Text>
+          <Text>{wallpapersListing[selectedPage].title}</Text>
+          <Text>Views : {wallpapersListing[selectedPage].viewsCount}</Text>
         </View>
-        <Pressable style={{flex: 0, alignSelf: 'center'}}>
+        <Pressable
+          style={{flex: 0, alignSelf: 'center'}}
+          onPress={() => {
+            setWallpapersListing(prev =>
+              prev.map((wallpaper, index) => {
+                if (index === selectedPage) {
+                  wallpaper.isLiked = !wallpapersListing[selectedPage].isLiked;
+                }
+                return wallpaper;
+              }),
+            );
+          }}>
           <Text style={{fontSize: 25}}>
-            {selectedPreviewWallpaper.isLiked ? 'Liked' : 'Like'}
+            {wallpapersListing[selectedPage].isLiked ? 'Liked' : 'Like'}
           </Text>
         </Pressable>
       </View>
-      <View style={{flex: 1}}>
-        <Image
-          source={{
-            uri: selectedPreviewWallpaper.uri,
-          }}
-          alt="image"
-          resizeMode="cover"
-          style={{width: '100%', height: '100%', borderRadius: 10}}
-        />
-      </View>
-      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+
+      <PagerView
+        style={{flex: 1}}
+        initialPage={selectedPage}
+        onPageSelected={e => {
+          setSelectedPage(e.nativeEvent.position);
+        }}>
+        {wallpapersListing.map(wallpaper => {
+          return <WallpaperPreviewBox uri={wallpaper.uri} key={wallpaper.id} />;
+        })}
+      </PagerView>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          paddingHorizontal: 20,
+        }}>
         <WallpaperPreviewOption />
         <WallpaperPreviewOption />
         <WallpaperPreviewOption />
@@ -45,6 +74,21 @@ const PreviewScreen = () => {
 };
 
 export default PreviewScreen;
+
+const WallpaperPreviewBox = ({uri}: {uri: string}) => {
+  return (
+    <View style={{flex: 1, paddingHorizontal: 20}}>
+      <Image
+        source={{
+          uri: uri,
+        }}
+        alt="image"
+        resizeMode="cover"
+        style={{width: '100%', height: '100%', borderRadius: 10}}
+      />
+    </View>
+  );
+};
 
 const WallpaperPreviewOption = () => {
   return (
